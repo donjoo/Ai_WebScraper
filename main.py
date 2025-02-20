6d09de828d1435f2ec58f6c4490d290899b94eef
donjoo <donjorois@gmail.com>
2025-02-20 22:57:00 +0530

    added what to scrape box

--- main.py
+++ main.py
@@ -14,4 +14,14 @@ if st.button("Scrape Site"):
 
     with st.expander("View Dom Content"):
         st.text_area("Dom Content", cleaned_content, height = 300)
-        
+
+
+if "dom_content"  in st.session_state:
+    prase_description= st.text_area("Describe what you want to prase?")
+    if st.button("Prase Content"):
+        if prase_description:
+            st.write("Prasing the content")
+
+            dom_chunks  = split_dom_content(st.session_state.dom_content)
+            
+
